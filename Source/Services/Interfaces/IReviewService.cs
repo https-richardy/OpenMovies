2bc9b3ea575a23062cb89fbf3998ca1dd4411d30
https-richardy <=>
2023-12-12 18:41:34 -0300

    #adjustment: GetReviewAsync method signature changed to support a predicate

--- Source/Services/Interfaces/IReviewService.cs
+++ Source/Services/Interfaces/IReviewService.cs
@@ -1,10 +1,11 @@
+using System.Linq.Expressions;
 using OpenMovies.Models;
 
 namespace OpenMovies.Services;
 
 public interface IReviewService
 {
-    Task<Review> GetReviewAsync(Review review);
+    Task<Review> GetReviewAsync(Expression<Func<Review, bool>> predicate);
     Task AddReviewAsync(Review review);
     Task UpdateReviewAsync(Review review);
     Task DeleteReviewAsync(Review review);
