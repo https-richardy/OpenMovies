bdbab2f562f9ad112291248ab6900e453643067e
https-richardy <=>
2023-12-15 18:57:51 -0300

    #feat: added new migrations for contains spoiler and classification feature in movie review feature

---
+++ Source/Data/Migrations/20231215215119_ContainsSpoilerAndClassificationFieldsInReviewModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenMovies.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContainsSpoilerAndClassificationFieldsInReviewModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Classification",
+                table: "Reviews",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ContainsSpoiler",
+                table: "Reviews",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Classification",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "ContainsSpoiler",
+                table: "Reviews");
+        }
+    }
+}
