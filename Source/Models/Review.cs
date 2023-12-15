3cab606716e1617fb203f96c55c755be25715c6c
https-richardy <=>
2023-12-15 18:50:05 -0300

    #feat: porperties 'classification' and 'contains spoiler' have been added to the Review model

--- Source/Models/Review.cs
+++ Source/Models/Review.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity;
 
 namespace OpenMovies.Models;
@@ -7,18 +8,24 @@ public class Review : Entity
     public bool Liked { get; set; }
     public string Comment { get; set; } = string.Empty;
 
+    [JsonIgnore]
     public Movie Movie { get; set; }
     public IdentityUser User { get; set; }
+
+    public Classification Classification { get; set; }
+    public bool ContainsSpoiler { get; set; }
 
     # pragma warning disable CS8618
     public Review() {  }  // Empty constructor for Entity Framework (CS8618)
 
     # pragma warning restore
-    public Review(bool liked, string comment, Movie movie, IdentityUser user)
+    public Review(bool liked, string comment, Movie movie, IdentityUser user, Classification classification, bool containsSpoiler)
     {
         Liked = liked;
         Comment = comment;
         Movie = movie;
         User = user;
+        Classification = classification;
+        ContainsSpoiler = containsSpoiler;
     }
 }
