7d433634c4b3af199f1056102530c4c66b53dc4d
https-richardy <=>
2023-12-15 23:58:00 -0300

    #feat: added more properties in ReviewDTO (classification and contains spoiler props)

--- Source/Models/DTOs/ReviewDTO.cs
+++ Source/Models/DTOs/ReviewDTO.cs
@@ -1,7 +1,12 @@
+using OpenMovies.Models;
+
 namespace OpenMovies.DTOs;
 
 public class ReviewDTO
 {
     public bool Liked { get; set; }
+    public bool ContainsSpoiler { get; set; }
+
     public string Comment { get; set; } = string.Empty;
+    public Classification Classification { get; set; }
 }
