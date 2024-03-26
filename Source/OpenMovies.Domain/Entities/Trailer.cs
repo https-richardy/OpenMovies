e7e5c4087878aa23988d4a7fe7482974f4fca6a5
https-richardy <=>
2024-03-25 22:31:17 -0300

    refactor(entities): remove Movie property from Trailer class

--- Source/OpenMovies.Domain/Entities/Trailer.cs
+++ Source/OpenMovies.Domain/Entities/Trailer.cs
@@ -1,21 +1,12 @@
-using System.Text.Json.Serialization;
-
 namespace OpenMovies.Domain.Enitites;
 
 public class Trailer : Entity
 {
-
-    [JsonIgnore]
-    public Movie Movie { get; set; }
     public string Link { get; set; } = string.Empty;
 
-    #pragma warning disable CS8618
-    public Trailer() {  }  // Empty constructor for Entity Framework
-
-    public Trailer(string link, Movie movie)
+    public Trailer(string link)
     {
         Link = GenerateEmbeddedLink(link);
-        Movie = movie;
     }
 
     private string GenerateEmbeddedLink(string link)
